9f3df6d317422184687c3b2fc337be627f575d2d
Wen Hai <hai.wen@haiwen-mac.local>
2021-06-29 22:22:11 +0800

    add lru mem limitation

--- lru.go
+++ lru.go
@@ -15,15 +15,20 @@ type LRUCache struct {
 	Elements map[string]*LRUElement
 	Size     int32
 	Capacity int32
+	MaxM     int32 //Bytes
 }
 
-func NewLRUCache(capacity int32) *LRUCache {
+func NewLRUCache(capacity, maxM int32) *LRUCache {
 	cache := &LRUCache{
 		Head:     &LRUElement{},
 		Tail:     &LRUElement{},
 		Elements: make(map[string]*LRUElement),
 		Size:     0,
 		Capacity: capacity,
+		MaxM:     maxM,
+	}
+	if cache.MaxM <= 0 {
+		cache.MaxM = 5 * 1024 * 1024 * 1025 //default 5GB
 	}
 	cache.Head.Prev = nil
 	cache.Head.Next = cache.Tail
